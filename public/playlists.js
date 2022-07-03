3c1a1e43f42598aee0ec493cd3a14f5ddc9348a2
Josh Phang <jphang@sfu.ca>
2022-07-03 12:43:44 -0700

    created a playlist display for each user. number of playlists can be changes using limit variable. currently using a hardcoded user id and auth token

--- public/playlists.js
+++ public/playlists.js
@@ -1,12 +1,43 @@
+// var clientID = "0f6749aefe004361b5c218e24c953814";
+// var clientSecret = "4940d82140ff4e47add12d60060cbcbc";
+// var redirect_uri = "http://localhost:5000/playlists.html";
+
 var apiToken =
-  "BQDeQ-IP-ORQ0ukrZww_-o7ILThiEgCc5qxImNIK5OvcVLaf9_0GDT9R_2bZoJ82MwuAaeqGzvxyqWsvDjkq06d76jgKun8fFFasVsoZPvgK-d-tut8sLWnzqqPhyZ5vYij9mQqMnF-T6Z-mtNQLk_6t319lSJ6YqvHpsJCkSq84bsB9BqjpeEh36HbDn7x8rX-lGTwIB9Rm_7lv";
+  "BQDKnH82PW8wm341YLWaoYRIyXQ34g8G6KaphYWYTyDnNKPZCKUYILXXDnwAmvdv_kVUx0lfJMUzQ42J6QWEv5Mbxdpsygf1ATD_456JRuUBXv7HNrbN7qkP-8o88jaBYN_bEs3QszoiLFslmTSMOhirthCvjEncxl6jP0st2Mf1b9_LvdxT12mCxX3PVs3XzLzVhQ7wTVy-3ya6MUWMZRxl5HUS-g";
 var playlist_id = "64RVqDV1d6MJUaecA0qLzh";
+var user_id = "22yveymmku7jub4aafyfdmlya";
 
 function test() {
-  getPlaylist(playlist_id);
+  // getPlaylistTracks(playlist_id);
+  getUserPlaylists(user_id, 6);
 }
 
-const getPlaylist = async (playlist_id) => {
+const getUserPlaylists = async (user_id, limit) => {
+  const result = await fetch(
+    `https://api.spotify.com/v1/users/${user_id}/playlists?limit=${limit}`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + apiToken },
+    }
+  );
+  const data = await result.json();
+  console.log(data);
+
+  for (let i = 0; i < data.items.length; i++) {
+    const playlistDisplay = document.createElement("div");
+    playlistDisplay.setAttribute("class", "playlist");
+    if (data.items[i].images[1] != null) {
+      playlistDisplay.style.backgroundImage = `url(${data.items[i].images[1].url})`;
+    } else {
+      playlistDisplay.style.backgroundImage = `url(${data.items[i].images[0].url})`;
+    }
+    playlistDisplay.innerHTML = data.items[i].name;
+    document.getElementById("wrapper").appendChild(playlistDisplay);
+    console.log("Name: " + data.items[i].name);
+  }
+};
+
+const getPlaylistTracks = async (playlist_id) => {
   const result = await fetch(
     `https://api.spotify.com/v1/playlists/${playlist_id}?market=CA`,
     {
@@ -29,6 +60,8 @@ const getPlaylist = async (playlist_id) => {
     }
   }
 };
+
+// function getToken() {}
 
 function msToMinSec(duration_ms) {
   return (
