624eb93da182312b33e5e6ae4643c769560b15a1
dqvo13 <dqvo13@gmail.com>
2022-07-03 19:39:25 -0700

    adjust spotify endpoint names

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const qs = require('qs');
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+// connect to postgreSQL
+const { Pool } = require('pg');
+const pool = new Pool({
+  // localhost server
+  // connectionString: 'schema://user:password@host/database'
+
+  // heroku server
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
@@ -24,35 +37,38 @@ app.get('/', (req, res) => {
     res.render('pages/start-page');
 });
 
-/************************* SPOTIFY OAUTH ROUTING *****************************/
-var client_id = process.env.CLIENT_ID;
-var client_secret = process.env.CLIENT_SECRET;
-var redirect_uri = process.env.REDIRECT_URI;
-
-/**
- * generates a random string
- * @param {number} length 
- * @returns {string} the generated string
- */
- var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-};
+/********************** POSTGRES ACCOUNT SETUP *******************************/
 
 app.get('/newUser', (req, res) => {
     res.render('pages/add-user');
 })
 
-app.get('/addUser', (req, res) => {
+app.get('/addUser', async (req, res) => {
+    var userName = req.body.f_uname;
+    var firstName = req.body.f_firstName;
+    var lastName = req.body.f_lastName;
+    var age = req.body.f_age;
 
+    var query = `INSERT INTO userAcct (id, username, firstname, lastname, age) VALUES (DEFAULT, $1, $2, $3, $4)`;
+    var values = [userName, firstName, lastName, age];
+
+    var rows = await pool.query(query, values);
+    if (rows) {
+        console.log("successfully added user: ", userName);
+        res.cookie('persongify_auth', rows);
+        // res.render('pages/dashboard', rows);
+    }
+    else {
+        res.redirect('pages/add-user');
+    }
 })
 
-app.get('/login', (req, res) => {
+/************************* SPOTIFY OAUTH ROUTING *****************************/
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
+var redirect_uri = process.env.REDIRECT_URI;
+
+app.get('/spotify-login', (req, res) => {
     
   var state = generateRandomString(16);
   var scope = 'user-read-private user-read-email user-library-modify user-library-read playlist-modify-private playlist-modify-public playlist-read-private user-top-read user-read-recently-played user-follow-read user-follow-modify';
@@ -69,7 +85,7 @@ app.get('/login', (req, res) => {
       }))
 })
 
-app.get('/callback', (req, res, next) => {
+app.get('/spotify-callback', (req, res, next) => {
 
   // given from login redirect
   var code = req.query.code || null;
@@ -105,6 +121,27 @@ app.get('/callback', (req, res, next) => {
           })
   }
 })
+
+/************************* HELPER FUNCTIONS **********************************/
+
+/**
+ * generates a random string
+ * @param {number} length 
+ * @returns {string} the generated string
+ */
+ var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+var checkAuthorizedUser = function() {
+
+}
  
 // Start the server
 const PORT = process.env.PORT || 5000;
