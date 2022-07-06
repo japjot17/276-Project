61755538fdd162eb259d2aec2291256b535d7664
timothyshyong <timshyong@gmail.com>
2022-07-05 17:18:53 -0700

    added autoplay option

--- public/carousel.js
+++ public/carousel.js
@@ -5,6 +5,7 @@ const slideIcons = document.querySelectorAll(".icon");
 const numberOfSlides = slides.length;
 var slideNumber = 0;
 
+// Next Button
 nextBtn.addEventListener("click", () => {
     slides.forEach((slide) => {
         slide.classList.remove("active");
@@ -23,6 +24,7 @@ nextBtn.addEventListener("click", () => {
   slideIcons[slideNumber].classList.add("active");
 });
 
+// Prev Button
 prevBtn.addEventListener("click", () => {
     slides.forEach((slide) => {
         slide.classList.remove("active");
@@ -40,3 +42,28 @@ prevBtn.addEventListener("click", () => {
   slides[slideNumber].classList.add("active");
   slideIcons[slideNumber].classList.add("active");
 })
+
+// Auto Play
+var autoplay;
+
+var loopThrough = () => {
+    autoplay = setInterval(function() {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+        slideIcons.forEach((slideIcon) => {
+            slideIcon.classList.remove("active");
+        });
+
+        slideNumber++;
+
+        if (slideNumber > (numberOfSlides -1)) {
+            slideNumber = 0;
+        }
+
+        slides[slideNumber].classList.add("active");
+        slideIcons[slideNumber].classList.add("active");
+    }, 5000);
+}
+
+loopThrough();
