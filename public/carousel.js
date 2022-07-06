b7b9a4fdbd8941dd2a6f34498a9540f345154322
timothyshyong <timshyong@gmail.com>
2022-07-05 17:32:03 -0700

    Added the ability to stop the image carousel autoplay when mousing over the next or previous button. Will resume autoplay when mouseout

--- public/carousel.js
+++ public/carousel.js
@@ -7,6 +7,7 @@ var slideNumber = 0;
 
 // Next Button
 nextBtn.addEventListener("click", () => {
+    // remove active class from all slides and icons
     slides.forEach((slide) => {
         slide.classList.remove("active");
     });
@@ -14,18 +15,22 @@ nextBtn.addEventListener("click", () => {
         slideIcon.classList.remove("active");
     });
     
+    // increment the slideNumber
     slideNumber++;
 
+    // ensure slides loop around
     if (slideNumber > (numberOfSlides - 1)){
         slideNumber = 0;
     }
 
-  slides[slideNumber].classList.add("active");
-  slideIcons[slideNumber].classList.add("active");
+    // add active class to a slide/icon to display/highlight them
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
 });
 
 // Prev Button
 prevBtn.addEventListener("click", () => {
+    // remove active class from all slides and icons
     slides.forEach((slide) => {
         slide.classList.remove("active");
     });
@@ -33,21 +38,25 @@ prevBtn.addEventListener("click", () => {
         slideIcon.classList.remove("active");
     });
     
+    // decrease the slideNumber by 1
     slideNumber--;
 
+    // ensure slides loop around
     if (slideNumber < 0){
         slideNumber = numberOfSlides -1;
     }
 
-  slides[slideNumber].classList.add("active");
-  slideIcons[slideNumber].classList.add("active");
+    // add active class to a slide/icon to display/highlight them
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
 })
 
-// Auto Play
-var autoplay;
+// Automatically shuffle through the slides
+var currentInterval;
 
 var loopThrough = () => {
-    autoplay = setInterval(function() {
+    currentInterval = setInterval(function() {
+        // remove active class from all slides and icons
         slides.forEach((slide) => {
             slide.classList.remove("active");
         });
@@ -55,15 +64,34 @@ var loopThrough = () => {
             slideIcon.classList.remove("active");
         });
 
+        // increase the slideNumber by 1
         slideNumber++;
 
+        // ensure slides loop around
         if (slideNumber > (numberOfSlides -1)) {
             slideNumber = 0;
         }
 
+        // add active class to a slide/icon to display/highlight them
         slides[slideNumber].classList.add("active");
         slideIcons[slideNumber].classList.add("active");
-    }, 5000);
+    }, 5000);   // switch slides every 5 seconds
 }
 
 loopThrough();
+
+// stop the image carousel autoplay when mousing over the next or previous button
+nextBtn.addEventListener("mouseover", () => {
+    clearInterval(currentInterval)
+});
+prevBtn.addEventListener("mouseover", () => {
+    clearInterval(currentInterval)
+});
+
+// restart the image slider autoplay after mouseout
+nextBtn.addEventListener("mouseout", () => {
+    loopThrough();
+})
+prevBtn.addEventListener("mouseout", () => {
+    loopThrough();
+})
