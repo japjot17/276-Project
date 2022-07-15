95f2b2ff6bdc81168a86b76937e7b48b8b7cb2b7
dqvo13 <dqvo13@gmail.com>
2022-07-15 16:47:41 -0700

    installed chai, chai-http

---
+++ tests/user-add-test.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../app.js");
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('database-test', () => {
+    
+})
