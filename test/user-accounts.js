02c3ea112d38e6f80560cc724bd8ef1013bde71e
dqvo13 <dqvo13@gmail.com>
2022-07-19 23:11:27 -0700

    fixed user account testing

--- test/user-accounts.js
+++ test/user-accounts.js
@@ -13,7 +13,7 @@ chai.use(chaiHttp);
 // e.g. req.should.have.param(parameter)
 
 describe("User Signup", () => {
-  it("should have added the user on POST /addUser", async () => {
+  it("should have added the user on POST /addUser", () => {
     var userInfo = {
       f_uname: "appTester123",
       f_firstName: "Mx.",
@@ -21,9 +21,18 @@ describe("User Signup", () => {
       f_age: 50,
       f_pwd: "testpwd123",
     };
-    var res = await chai.request(server).post("/addUser").send(userInfo);
-    res.should.have.cookie("persongify_auth");
-    res.should.have.status(302);
+    
+    // var res = await chai.request(server).post("/addUser").send(userInfo);
+    var agent = chai.request.agent(server);
+    agent
+      .post("/addUser")
+      .send(userInfo)
+      .then((res) => {
+        res.should.have.cookie("persongify_auth");
+        res.should.have.status(302);
+        res.should.redirect;
+      })
+    agent.close();
   });
 
   // TODO: write test for failed userAdd? (status 500)
@@ -35,9 +44,19 @@ describe("User Login", () => {
       f_uname: "appTester123",
       f_pwd: "testpwd123",
     };
-    var res = await chai.request(server).post("/verify-login").send(userInfo);
-    res.should.have.cookie("persongify_auth");
-    res.should.have.status(302);
+    // var res = chai.request(server).post("/verify-login").send(userInfo);
+    var agent = chai.request.agent(server);
+    agent
+      .post("/verify-login")
+      .send(userInfo)
+      .then((res) => {
+        res.should.have.cookie("persongify_auth");
+        res.should.have.status(302);
+        res.should.redirectTo(/^[\s\S]*\/spotify-login$/);
+      })
+    // res.should.have.cookie("persongify_auth");
+    // res.should.have.status(302);
+    agent.close();
   });
 
   it("should fail to log in on POST /verify-login", async () => {
@@ -45,34 +64,81 @@ describe("User Login", () => {
       f_uname: "randomStranger",
       f_pwd: "wrongPassword",
     };
-    var res = await chai.request(server).post("/verify-login").send(userInfo);
-    res.should.have.status(303);
+    // var res = await chai.request(server).post("/verify-login").send(userInfo);
+    var agent = chai.request.agent(server);
+    agent
+      .post("/verify-login")
+      .send(userInfo)
+      .then((res) => {
+        res.should.not.have.cookie("persongify_auth");
+        res.should.have.status(303);
+        res.should.redirectTo(/^[\s\S]*\/login$/)
+      })
+    agent.close();
   });
 });
 
 describe("User Logout", () => {
   it("should log out current user on GET /logout", async () => {
-    var res = await chai.request(server).get("/logout");
-    res.should.not.have.cookie("persongify_auth");
-    res.should.not.have.cookie("spotify_auth");
-    res.should.have.status(302);
+    // var res = await chai.request(server).get("/logout");
+    // res.should.not.have.cookie("persongify_auth");
+    // res.should.not.have.cookie("spotify_auth");
+    // // res.should.have.status(302);
+    // res.should.redirectTo(/^[\s\S]*\/home$/);
+    var agent = chai.request.agent(server);
+    agent
+      .get("/logout")
+      .then((res) => {
+        res.should.not.have.cookie("persongify_auth");
+        res.should.not.have.cookie("spotify_auth");
+        res.should.have.status(302);
+        res.should.redirectTo(/^[\s\S]*\/home$/);
+      })
+    agent.close();
   });
 });
 
 describe("Redirects for logged out users", () => {
   it("should redirect to login on GET /spotify-login", async () => {
-    var res = await chai.request(server).get("/spotify-login");
-    res.should.not.have.cookie("persongify_auth");
-    res.should.have.status(303);
+    // var res = await chai.request(server).get("/spotify-login");
+    // res.should.not.have.cookie("persongify_auth");
+    // res.should.have.status(303);
+    var agent = chai.request.agent(server);
+    agent
+      .get("/spotify-login")
+      .then((res) => {
+        res.should.not.have.cookie("persongify_auth");
+        res.should.have.status(303);
+        res.should.redirectTo(/^[\s\S]*\/login$/);
+      })
+    agent.close();
   });
   it("should redirect to login on GET /trending", async () => {
-    var res = await chai.request(server).get("/trending");
-    res.should.not.have.cookie("persongify_auth");
-    res.should.have.status(303);
+    // var res = await chai.request(server).get("/trending");
+    // res.should.not.have.cookie("persongify_auth");
+    // res.should.have.status(303);
+    var agent = chai.request.agent(server);
+    agent
+      .get("/trending")
+      .then((res) => {
+        res.should.not.have.cookie("persongify_auth");
+        res.should.have.status(303);
+        res.should.redirectTo(/^[\s\S]*\/login$/);
+      })
+    agent.close();
   });
   it("should redirect to login on GET /songs", async () => {
-    var res = await chai.request(server).get("/songs");
-    res.should.not.have.cookie("persongify_auth");
-    res.should.have.status(303);
+    // var res = await chai.request(server).get("/songs");
+    // res.should.not.have.cookie("persongify_auth");
+    // res.should.have.status(303);
+    var agent = chai.request.agent(server);
+    agent
+      .get("/songs")
+      .then((res) => {
+        res.should.not.have.cookie("persongify_auth");
+        res.should.have.status(303);
+        res.should.redirectTo(/^[\s\S]*\/login$/);
+      })
+    agent.close();
   });
 });
