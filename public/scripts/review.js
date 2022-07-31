2f64c80f7a83b00d1ecab1d31048a39294818426
Joshua Phang <jphang@sfu.ca>
2022-07-31 13:48:19 -0700

    testing with facebook api for login

--- public/scripts/review.js
+++ public/scripts/review.js
@@ -138,4 +138,45 @@ const addTracksToPlaylist = async (playlist_id) => {
     } else {
       document.getElementById("playlist-status-msg").innerHTML = "Unsuccessful. Please try again.";
     }
-  };
+};
+
+
+/** Asynchronously loads Facebook SDK */
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '1169143580322957',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v14.0'
+    });
+        
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+        statusChangeCallback(response);        // Returns the login status.
+    }); 
+};
+
+function testAPI() {   
+    // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+}
+
+function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      console.log("couldn't login to facbeook ;("); 
+    }
+  }
